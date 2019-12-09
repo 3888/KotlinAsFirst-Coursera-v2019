a4f1256783e05e47b87c4998e38b86037cef833d
Anton <krrg@mail.ru>
2019-12-09 16:22:53 +0200

    added solutions for Lesson2

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -3,6 +3,9 @@
 package lesson2.task2
 
 import lesson1.task1.sqr
+import lesson1.task1.trackLength
+import java.time.YearMonth
+import kotlin.math.abs
 
 /**
  * Пример
@@ -13,33 +16,49 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
     sqr(x - x0) + sqr(y - y0) <= sqr(r)
 
 /**
- * Простая
+ * Простая -
  *
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
+fun isNumberHappy(number: Int): Boolean {
+
+    val isHappy = number.div(1000) + number.div(100).rem(10) == number.div(10).rem(10) + number.rem(10)
+
+//    val numbers = number.toString().map { it.toString().toInt() }
+//    val isHappy =  numbers[0] + numbers[1] == numbers[2] + numbers[3]
+
+    return isHappy
+}
+
 
 /**
- * Простая
+ * Простая +
  *
  * На шахматной доске стоят два ферзя (ферзь бьет по вертикали, горизонтали и диагоналям).
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  * Считать, что ферзи не могут загораживать друг друга.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean =
+    when {
+        (abs(x1 - x2) == abs(y1 - y2)) -> true
+        (x1 == x2 || y1 == y2) -> true
+        else -> false
+    }
 
 
 /**
- * Простая
+ * Простая +
  *
  * Дан номер месяца (от 1 до 12 включительно) и год (положительный).
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
-fun daysInMonth(month: Int, year: Int): Int = TODO()
+fun daysInMonth(month: Int, year: Int): Int {
+    return YearMonth.of(year, month).lengthOfMonth()
+}
 
 /**
- * Средняя
+ * Средняя +
  *
  * Проверить, лежит ли окружность с центром в (x1, y1) и радиусом r1 целиком внутри
  * окружности с центром в (x2, y2) и радиусом r2.
@@ -48,10 +67,14 @@ fun daysInMonth(month: Int, year: Int): Int = TODO()
 fun circleInside(
     x1: Double, y1: Double, r1: Double,
     x2: Double, y2: Double, r2: Double
-): Boolean = TODO()
+): Boolean {
+    val d = trackLength(x1, y1, x2, y2)
+
+    return d <= r2 - r1
+}
 
 /**
- * Средняя
+ * Средняя +
  *
  * Определить, пройдет ли кирпич со сторонами а, b, c сквозь прямоугольное отверстие в стене со сторонами r и s.
  * Стороны отверстия должны быть параллельны граням кирпича.
@@ -59,4 +82,23 @@ fun circleInside(
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = TODO()
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean {
+    val holeSquare = r * s
+    val brickSquareFace = a * b
+    val brickSquareSideFace = a * c
+    val brickSquareOnEdge = b * c
+
+    var appropriateSize = false
+
+    if (brickSquareFace <= holeSquare) {
+        appropriateSize = true
+    }
+    if (brickSquareSideFace <= holeSquare) {
+        appropriateSize = true
+    }
+    if (brickSquareOnEdge <= holeSquare) {
+        appropriateSize = true
+    }
+
+    return appropriateSize
+}
